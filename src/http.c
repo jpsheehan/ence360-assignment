fc5f4cff15881b7740f56150f4131ce8eb1ae8e4
JP Sheehan <jesse@sheehan.nz>
2019-08-01 21:38:42 +1200

    Added some helper functions for buffer stuff.

--- src/http.c
+++ src/http.c
@@ -11,8 +11,67 @@
 
 #define BUF_SIZE 1024
 
+#define HEADER_USER_AGENT "ENCE360/1.0 (assignment)"
 
+enum http_method
+{
+    GET,
+    POST,
+    HEAD
+};
+typedef enum http_method HttpMethod;
 
+/**
+ * Creates a buffer with size t_initial_size bytes.
+ * Returns a pointer to the buffer or NULL upon failure.
+ */
+Buffer *buffer_create(size_t t_initial_size)
+{
+    Buffer *buffer = malloc(sizeof(Buffer));
+
+    if (buffer)
+    {
+        buffer->data = malloc(t_initial_size);
+
+        if (buffer->data == NULL)
+        {
+            free(buffer);
+            buffer = NULL;
+        }
+        else
+        {
+            buffer->length = t_initial_size;
+        }
+    }
+
+    return buffer;
+}
+
+/**
+ * Doubles the capacity of the buffer.
+ * Returns 0 on success, -1 on failure.
+ */
+int buffer_double_size(Buffer *t_buffer)
+{
+    size_t new_length = t_buffer->length * 2;
+    char *new_data = realloc(t_buffer->data, new_length);
+
+    if (new_data != t_buffer->data)
+    {
+        t_buffer->data = new_data;
+        t_buffer->length = new_length;
+        return 0;
+    }
+    else
+    {
+        return -1;
+    }
+}
+
+Buffer *http_raw_query(char *host, char *path, HttpMethod method, const char *range, int port)
+{
+    assert(0 && "not implemented yet!");
+}
 
 /**
  * Perform an HTTP 1.0 query to a given host and page and port number.
@@ -27,10 +86,10 @@
  * @return Buffer - Pointer to a buffer holding response data from query
  *                  NULL is returned on failure.
  */
-Buffer* http_query(char *host, char *page, const char *range, int port) {
-    assert(0 && "not implemented yet!");
+Buffer *http_query(char *host, char *page, const char *range, int port)
+{
+    return http_raw_query(host, page, GET, range, port);
 }
-
 
 /**
  * Separate the content from the header of an http request.
@@ -40,18 +99,20 @@ Buffer* http_query(char *host, char *page, const char *range, int port) {
  *                   content from
  * @return string response or NULL on failure (buffer is not HTTP response)
  */
-char* http_get_content(Buffer *response) {
+char *http_get_content(Buffer *response)
+{
 
-    char* header_end = strstr(response->data, "\r\n\r\n");
+    char *header_end = strstr(response->data, "\r\n\r\n");
 
-    if (header_end) {
+    if (header_end)
+    {
         return header_end + 4;
     }
-    else {
+    else
+    {
         return response->data;
     }
 }
-
 
 /**
  * Splits an HTTP url into host, page. On success, calls http_query
@@ -60,25 +121,27 @@ char* http_get_content(Buffer *response) {
  * @param range - The desired byte range of data to retrieve from the page
  * @return Buffer pointer holding raw string data or NULL on failure
  */
-Buffer *http_url(const char *url, const char *range) {
+Buffer *http_url(const char *url, const char *range)
+{
     char host[BUF_SIZE];
     strncpy(host, url, BUF_SIZE);
 
     char *page = strstr(host, "/");
-    
-    if (page) {
+
+    if (page)
+    {
         page[0] = '\0';
 
         ++page;
         return http_query(host, page, range, 80);
     }
-    else {
+    else
+    {
 
         fprintf(stderr, "could not split url into host/page %s\n", url);
         return NULL;
     }
 }
-
 
 /**
  * Makes a HEAD request to a given URL and gets the content length
@@ -88,11 +151,12 @@ Buffer *http_url(const char *url, const char *range) {
  * @return int  The number of downloads needed satisfying max_chunk_size
  *              to download the resource
  */
-int get_num_tasks(char *url, int threads) {
-   assert(0 && "not implemented yet!");
+int get_num_tasks(char *url, int threads)
+{
+    assert(0 && "not implemented yet!");
 }
 
-
-int get_max_chunk_size() {
+int get_max_chunk_size()
+{
     return max_chunk_size;
 }
